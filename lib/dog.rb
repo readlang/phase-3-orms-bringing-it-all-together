e916c633dab9bed3dcac36cc81c0a2311ec96f13
Read Langworthy <rlangworthy@gmail.com>
2022-02-28 12:38:20 -0800

    commit tree 00a0c193c0e42ef1200ccd56f50d3602c04567ec

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,58 @@
 class Dog
+    attr_accessor :name, :breed, :id 
+
+    def initialize(name:, breed:, id: nil)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table
+        DB[:conn].execute("CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)")
+    end
+
+    def self.drop_table
+        DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+    end
+
+    def save
+        sql = <<-SQL
+            INSERT INTO dogs (name, breed)
+            VALUES (?,?)
+        SQL
+
+        DB[:conn].execute(sql, self.name, self.breed)
+        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+        self
+    end
+
+    def self.create(name:, breed:)
+        dog = Dog.new(name: name, breed: breed)
+        dog.save
+    end
+
+    def self.new_from_db(row)
+        self.new(id: row[0], name: row[1], breed: row[2])
+    end
+
+    def self.all
+        DB[:conn].execute("SELECT * FROM dogs").map do |row|
+            self.new_from_db(row)
+        end
+        
+    end
+
+    def self.find_by_name(name)
+        DB[:conn].execute("SELECT * FROM dogs WHERE name = ? LIMIT 1", name).map do |row|
+            self.new_from_db(row)
+        end.first
+    end
+
+    def self.find(id)
+        DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", id).map do |row|
+            self.new_from_db(row)
+        end.first
+    end
 
 end
